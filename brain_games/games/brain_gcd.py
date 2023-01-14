67321fb834f30306da44202bc1756f3ff10829aa
Ivan Berdigulov <ivan.berdigulov@gmail.com>
2023-01-15 00:50:06 +0300

    add brain-gcd

---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,16 @@
+from random import randint
+from brain_games.game_pattern import ans_que
+
+condition = 'Find the greatest common divisor of given numbers.'
+
+def main_game(name):
+    int_1 = randint(1, 99)
+    int_2 = randint(1, 99)
+    question = f'{int_1} {int_2}'
+    while int_1 != int_2:
+        if int_1 > int_2:
+            int_1 -= int_2
+        else:
+            int_2 -= int_1
+    err = ans_que(question, int_1, name)
+    return err
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,17 @@
+import prompt
+from brain_games.games.brain_gcd import main_game, condition
+
+
+def main():
+    print('Welcome to the Brain games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print(condition)
+    count = 0
+    for _ in range(3):
+        err = main_game(name)
+        count += 1
+        if err is True:
+            break
+    if count == 3:
+        print(f'Congratulations, {name}!')
